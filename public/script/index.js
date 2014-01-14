e7c8c926bcbcb49da7f1f659e2e1408d892cb27c
Jordan <skorlir@gmail.com>
2014-01-14 01:17:29 -0600

    added parallaxy goodness on mousemove, edited blur effect and rearranged other stuff

--- public/script/index.js
+++ public/script/index.js
@@ -1,4 +1,28 @@
 $(document).ready(function () {
-   $('button').click(function () {
+
+  var body = $('body');
+	var overlay = $('.banner');
+	
+	$('button').click(function () {
    });
+	
+	var winwh, winhh;
+	
+	winwh = Math.floor($(window).width()/8);
+	winhh = Math.floor($(window).height()/6);
+	$.easing.smoothishmove = function (x, t, b, c, d) {	
+		return -c *(t/=d)*(t-2) + b;
+ 	}; 
+	
+	function parallalax(e) { 
+		body.animate({
+   'background-position-x': -winwh + (e.pageX-winwh)/10,
+   'background-position-y': -winhh + (e.pageY-winhh)/10
+ }, {queue:false,duration:50,easing:'smoothishmove'});
+		console.log((winwh - e.pageX)/winwh + ' ' + (winhh - e.pageY)/winhh);
+		}
+	
+	$(document).mousemove(function (e) {
+		parallalax(e);
+	});
 });
