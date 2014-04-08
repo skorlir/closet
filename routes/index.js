09ffa6d2d4d8aff124e118fafde3bddac85dbeeb
Jordan <skorlir@gmail.com>
2014-04-08 00:40:35 -0500

    strip to bare-bones and setup landing page w/ mailchimp and facebook page

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,5 @@
 var Items = require('../models/Item.js');
 
 exports.index = function(req, res){
-	if (req.isAuthenticated()) {
-		Items.find().limit(10).exec(function(err, result) {
-			if (err) console.log(err);
-			res.render('dashboard', { user: req.user, items: result });
-		});
-	}
-	else {
-		res.render('index', {title: 'Closet'});
-	}
+	res.render('index', {title: 'Closet'});
 };
