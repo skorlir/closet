ec676596b741b07a7b2a372f8c6bfe3650d3bf32
skorlir <skorlir@gmail.com>
2014-05-27 04:43:14 -0500

    move northwestern email logic into base controller

--- public/script/angular/controllers/base.js
+++ public/script/angular/controllers/base.js
@@ -1,4 +1,4 @@
-app.controller('base', ['$scope', 'sessionService', '$window', '$modal', '$http', '$position', function($scope, session, $window, $modal, $http) {
+app.controller('base', ['$scope', 'sessionService', '$window', '$modal', '$http', '$filter', '$position', function($scope, session, $window, $modal, $http, $filter) {
 
   $scope.fbLogin = session.fbLogin(function(user) {
     console.log('logged in as', user.uid, user.displayName);
@@ -25,6 +25,23 @@ app.controller('base', ['$scope', 'sessionService', '$window', '$modal', '$http'
     
     modal.result.then(function() {
       $http.post('/nonfeature', {data: thing});
+    });
+  }
+  
+  session.getUser(function(user) {
+    $scope.hasNUEmail = function() {
+        var email = [];
+        if(user.restrictions) email = user.restrictions.email;
+        else email = [user.email];
+        email = $filter('orderByPriority')(email);
+        return !!email.filter(function(em) { return em.indexOf('northwestern.edu') > -1; }).length;
+    }
+  });
+  
+  $scope.addNorthwesternEmail = function(email) {
+    session.getUser(function(user) {
+      db.addRestrictionProp('email', email, user.uid);
+      restrictionService.setUser(user);
     });
   }
 }]);
